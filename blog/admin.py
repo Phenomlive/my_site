00c36fa35e869221e6762e4d494b26bae3cd26df
Fuad <fuadreddhow@gmail.com>
2025-01-07 17:13:19 +0600

    added tags and mail link to templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'author',)
+    list_filter = ('date', 'author', 'tags',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
