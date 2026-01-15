3dd3a6dbac2946b9fc41d6c19ecd9a1a2b197d1e
Fuad <fuadreddhow@gmail.com>
2026-01-16 02:15:48 +0600

    fixed issues with env variables

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('IS_DEVELOPMENT', True)
+DEBUG = os.getenv('IS_DEVELOPMENT', True)
 
 ALLOWED_HOSTS = [
-    getenv('APP_HOST', 'localhost')
+    os.getenv('APP_HOST', 'localhost')
 ]
 
 
