dab132b4eae11b9cbc18eab1ef46138468bc1d66
Fuad <fuadreddhow@gmail.com>
2026-01-17 13:17:40 +0600

    fixed some errors which led to deployment failure on render

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --workers=2 --worker-class=sync --worker-tmp-dir=/dev/shm --max-requests=1000 my_site.wsgi
+web: gunicorn --workers=2 --max-requests=1000 my_site.wsgi
--- my_site/settings.py
+++ my_site/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-import warnings
-import psycopg2
+
 
 load_dotenv()
 
